1449da5a293d15b5a63764fa1a5b7b722e40f638
Carlos Magno <cmagno@ufrnet.br>
2018-08-24 13:48:27 -0300

    Alteracaoes 04 script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $.getJSON("database.json", function(data) {
     saida += '<div class="row">';
     saida += '<div class="col-lg-4 band-img">';
     saida += '<img src="' + bands[i].picture + '" alt="' + bands[i].name + '" title="' + bands[i].name + '">';
-    saida += '<h1> ' + bands[i].description + '</h1>';
+    saida += '<h6> ' + bands[i].description + '</h6>';
     saida += '</div>';
     saida += '</div>';
   }
